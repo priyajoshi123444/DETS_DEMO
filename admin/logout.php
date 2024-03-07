15fbf5c2eaadf0e5fc01ce64862b8e2151ebef1c
SakshiKagda <156991393+SakshiKagda@users.noreply.github.com>
2024-03-07 10:36:06 +0530

    Delete admin directory

--- admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();
-session_destroy();
-echo "<script>alert('Are you sure you want to log out?'); window.location.href = 'login.php';</script>";
-exit();
-?>
